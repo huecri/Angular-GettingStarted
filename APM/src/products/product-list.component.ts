3248948f171dbb7e4874e9ca15b129425f398fee
huecri <richard.hueck@grenzebach.com>
2023-07-13 13:42:06 +0200

    Subscribe
     to the product service to get products updated

--- APM/src/products/product-list.component.ts
+++ APM/src/products/product-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'pm-products',
     templateUrl: "./product-list.component.html",
     styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
+    errorMessage = '';
+    sub!: Subscription;
 
     constructor(private productService: ProductService) {
     }
+
 
     onRatingClicked(message: string) : void {
         this.pageTitel = "Product List: " + message;
@@ -42,8 +46,14 @@ export class ProductListComponent implements OnInit {
 
     products: IProduct[] = [];
 
-        ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts()
+        .subscribe({next: products => {
+            this.products = products;
+            this.filteredProducts = this.products;
+        } , error: err => this.errorMessage = err});
+    }
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 }

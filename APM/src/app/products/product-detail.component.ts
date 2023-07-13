f62f4bda37351f5a5fc1218e1a836b50b67ed946
huecri <richard.hueck@grenzebach.com>
2023-07-13 15:45:48 +0200

    Add some  simple details view

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -14,7 +14,18 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    };
   }
+
   onBack(): void {
     this.router.navigate(['/products']);
   }

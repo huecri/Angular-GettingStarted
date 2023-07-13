ef3c0743054a9b8a27e73bb7db07a15b5e11b000
huecri <richard.hueck@grenzebach.com>
2023-07-13 11:06:54 +0200

    Use Star visualization

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from 'src/products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
+import { StartComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
+    StartComponent,
   ],
   imports: [
     BrowserModule,
---
+++ APM/src/app/shared/star.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: "./star.component.html",
+    styleUrls: ["./star.component.css"],
+
+})
+export class StartComponent implements OnChanges {
+    ngOnChanges(changes: SimpleChanges): void {
+        this.cropWidth = this.rating * 75/5;
+    }
+    @Input() rating = 4
+    cropWidth = 50;
+}
